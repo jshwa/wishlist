a4f1149e7c3984a0810b2f16501f56835ce53ddc
jshwa <32717724+jshwa@users.noreply.github.com>
2017-12-31 16:33:35 -0600

    build out create route to take in categories attributes and associate the gift with the current users list. Also strong params

--- app/controllers/gifts_controller.rb
+++ app/controllers/gifts_controller.rb
@@ -4,8 +4,23 @@ class GiftsController < ApplicationController
 
   def new
     @gift = Gift.new
+    @category = @gift.categories.build
+  end
+
+  def create
+    gift = Gift.new(gift_params)
+    gift.save
+    gift.lists << current_user.list
+    redirect_to list_path(current_user.list.id)
   end
 
   def edit
+
+  end
+
+  private
+
+  def gift_params
+    params.require(:gift).permit(:name, :description, lists: [], categories_attributes: [:name])
   end
 end
