cceaf535d99d5f34d2aaaa7e7f2fbe07f6b08c19
jshwa <32717724+jshwa@users.noreply.github.com>
2017-12-30 22:41:03 -0600

    added routes controller and views for wishlists

---
+++ app/controllers/wishlists_controller.rb
@@ -0,0 +1,13 @@
+class WishlistsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
   root to: "home#index"
   resources :gifts, only: [:index, :new, :edit, :show]
+  resources :wishlists, only: [:index, :new, :edit, :show]
+  
 end
