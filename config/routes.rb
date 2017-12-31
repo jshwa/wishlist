18c44c690d7406421d4d567f2e13e984d554ff00
jshwa <32717724+jshwa@users.noreply.github.com>
2017-12-30 22:37:27 -0600

    added routes controller and views for gifts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'} 
+
+  devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
   root to: "home#index"
+  resources :gifts, only: [:index, :new, :edit, :show]
 end
