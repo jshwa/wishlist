5cd35ea147098930ef257199b5106721af70b129
jshwa <32717724+jshwa@users.noreply.github.com>
2017-12-31 16:37:17 -0600

    added all restful routes for gifts and categories

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
   root to: "lists#index"
-  resources :gifts, only: [:index, :new, :edit, :show]
-  resources :lists, only: [:index, :new, :edit, :show]
-  resources :categories, only: [:index, :new, :edit, :show]
+  resources :gifts
+  resources :lists, only: [:index, :edit, :show]
+  resources :categories
 
 end
