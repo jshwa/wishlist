ddc43ffbb0478e0eb5857927e6cd2d039786716d
jshwa <32717724+jshwa@users.noreply.github.com>
2017-12-31 16:37:56 -0600

    fixed column in categories table, changed gifts_id to gift_id

--- db/migrate/20171230003908_create_category_gifts.rb
+++ db/migrate/20171230003908_create_category_gifts.rb
@@ -1,7 +1,7 @@
 class CreateCategoryGifts < ActiveRecord::Migration[5.1]
   def change
     create_table :category_gifts do |t|
-      t.integer :gifts_id
+      t.integer :gift_id
       t.integer :category_id
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20171230003908) do
   end
 
   create_table "category_gifts", force: :cascade do |t|
-    t.integer "gifts_id"
+    t.integer "gift_id"
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
