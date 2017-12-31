241744b77ac916cecf45b3fbe7098e05fd39f5d1
jshwa <32717724+jshwa@users.noreply.github.com>
2017-12-31 16:34:51 -0600

    created categories_attributes method to find or create attributes sent from the nested form

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -4,4 +4,10 @@ class Gift < ApplicationRecord
   has_many :users, through: :lists
   has_many :category_gifts
   has_many :categories, through: :category_gifts
+
+  def categories_attributes=(categories_attributes)
+    categories_attributes.each do |i, cat_attr|
+      self.categories << (Category.find_or_create_by(name: cat_attr[:name]))
+    end
+  end
 end
