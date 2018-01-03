f70c234d7146d589619a3329378ebec7a21411b0
jshwa <32717724+jshwa@users.noreply.github.com>
2018-01-03 16:31:10 -0600

    create route in reviews controller adds a new review from form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,22 @@
 class ReviewsController < ApplicationController
-  before_action :set_gift, only: [:index, :new]
+  before_action :set_gift, only: [:index, :new, :create]
 
   def index
   end
 
   def new
     @review = @gift.reviews.build
+  end
+
+  def create
+    review = @gift.reviews.build(review_params)
+    review.user = current_user
+    if review.save
+      redirect_to gift_review_path(@gift, review)
+    else
+      flash[:alert] = "There was a problem saving your review"
+      render new_gift_review(@gift)
+    end
   end
 
   def edit
@@ -15,6 +26,10 @@ class ReviewsController < ApplicationController
   end
 
   private
+
+  def review_params
+    params.require(:review).permit(:stars, :comment)
+  end
 
   def set_gift
     @gift = Gift.find_by(id: params[:gift_id])
