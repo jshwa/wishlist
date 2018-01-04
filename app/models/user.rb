6190bd71358f6952f4c2d264a8ba39c0e6047d75
jshwa <32717724+jshwa@users.noreply.github.com>
2018-01-03 22:07:20 -0600

    User.from_omniauth creates a random username for a facebook signup so the user can be persisted

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,9 +31,15 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.username = make_random_username(auth.info.name)
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
     end
+  end
+
+  def self.make_random_username(name)
+    num = Random.new
+    name.split(" ").first + num.rand(100000..999999).to_s
   end
 
   def wrote_review?(gift)
