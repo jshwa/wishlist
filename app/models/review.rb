c25d5ca0286fed6dc5470caa9602d9d2562c7bbc
jshwa <32717724+jshwa@users.noreply.github.com>
2018-01-06 02:01:34 -0600

    stars show rating instead of just number of stars

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :gift
+
+  def display_rating
+    rating = ""
+    stars.times { rating += "<i class='fa fa-star' aria-hidden='true'></i>"}
+    rating.html_safe
+  end
 end
