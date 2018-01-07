ec85d71db8636352abf981f9e0fdc60a9cf87520
jshwa <32717724+jshwa@users.noreply.github.com>
2018-01-07 00:11:42 -0600

    formatting for nav bar with hover, added route and link for user reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
-  resources :users, only: [:update]
-  get 'users/edit_username', to: 'users#edit_username', as: 'edit_username'
   root to: "lists#index"
+
+  devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
+
+  resources :users, only: [:update] do
+    resources :reviews, only: [:index]
+  end
+
+  get 'users/edit_username', to: 'users#edit_username', as: 'edit_username'
+
   resources :lists, only: [:index, :edit, :show]
   resources :categories
+  
   resources :gifts, only: [:index, :show, :new, :create, :update, :destroy] do
     resources :reviews
   end
