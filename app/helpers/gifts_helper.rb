aed9eaefa1e4f10a8467a4cbc46ceadbdca2540f
jshwa <32717724+jshwa@users.noreply.github.com>
2018-01-08 13:10:05 -0600

    formatting for search results to be in card format, added helper method sorten name to cut names of products if they are >150 characters

---
+++ app/helpers/gifts_helper.rb
@@ -0,0 +1,12 @@
+module GiftsHelper
+
+  def shorten_name(name)
+    if name.length > 150
+      name_arr = name.reverse.split("").slice_when {|char| char =~ /[\,\.\-]/}.to_a
+      name_arr.shift
+      shorten_name(name_arr.flatten.reverse.join(""))
+    else
+      name
+    end
+  end
+end
