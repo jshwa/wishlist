fc3efca2108255afa07ac8a89b40a35e7942b308
jshwa <32717724+jshwa@users.noreply.github.com>
2018-01-08 21:50:51 -0600

    resized default image, fixed image displaying under wishlists list for current user

--- app/decorators/list_decorator.rb
+++ app/decorators/list_decorator.rb
@@ -2,9 +2,11 @@ class ListDecorator < Draper::Decorator
   delegate_all
 
   def display_users
-    h.content_tag :li do
-      h.concat h.link_to_if list.user != h.current_user, (h.image_tag list.user.image), h.list_path(list)
-      h.concat h.link_to_if list.user != h.current_user, list.user.username, h.list_path(list)
+    if list.user != h.current_user
+      h.content_tag :li do
+        h.concat h.link_to (h.image_tag list.user.image), h.list_path(list)
+        h.concat h.link_to list.user.username.humanize, h.list_path(list)
+      end
     end
   end
 
