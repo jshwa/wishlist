da3a07062fb84cbbb14da4b61a19eff571be77f7
jshwa <32717724+jshwa@users.noreply.github.com>
2018-01-10 00:20:04 -0600

    moved add_a_button method to helper

--- app/helpers/gifts_helper.rb
+++ app/helpers/gifts_helper.rb
@@ -9,4 +9,12 @@ module GiftsHelper
       name
     end
   end
+
+  def add_a_button(gift)
+    if current_user && current_user.gifts.include?(gift)
+      button_to "On Your Wishlist", list_path(current_user.list), method: 'get', class: 'item_button_added'
+    else
+      button_to "Add to Wishlist", gift_path(gift), method:'patch', class:'item_button'
+    end
+  end
 end
--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -3,5 +3,4 @@ module ReviewsHelper
   def formatted_updated_at(review)
     review.updated_at.strftime("%b %d, %Y")
   end
-
 end
