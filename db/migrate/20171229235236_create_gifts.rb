da6b343368b9d88cf678066056fc102041e20c87
jshwa <32717724+jshwa@users.noreply.github.com>
2018-01-10 19:01:59 -0600

    added default gift image to gift table and added background image

--- db/migrate/20171229235236_create_gifts.rb
+++ db/migrate/20171229235236_create_gifts.rb
@@ -2,7 +2,7 @@ class CreateGifts < ActiveRecord::Migration[5.1]
   def change
     create_table :gifts do |t|
       t.string :name
-      t.string :url
+      t.string :url, default: "default_gift.jpg"
       t.string :description
       t.decimal :price, precision: 8, scale: 2
       t.string :image
