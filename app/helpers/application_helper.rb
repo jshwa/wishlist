ac18e21c435332485a3aba7113ae2b4d91837cd7
jshwa <32717724+jshwa@users.noreply.github.com>
2018-01-12 02:17:35 -0600

    moved logic for showing errors to helper, fixed formatting for sign up and login pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def show_errors
+    unless request.path == "/users/sign_in"
+      if flash[:notice] || flash[:alert]
+        "<p class='notice'>#{notice}</p>"
+        "<p class='alert'>#{alert}</p>"
+      end.try(:html_safe)
+    end
+  end
 end
