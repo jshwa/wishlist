856b714f68c61381735b8a1e249ed3ac58cf1de2
jshwa <32717724+jshwa@users.noreply.github.com>
2018-01-12 00:32:45 -0600

    add write a review button if user hasn't written on yet, even if there are other reviews

--- app/decorators/reviews_decorator.rb
+++ app/decorators/reviews_decorator.rb
@@ -6,13 +6,17 @@ class ReviewsDecorator < Draper::CollectionDecorator
 
   def no_reviews(gift, user)
     if object.length == 0
-      h.content_tag :div, class: "review_item" do
+      h.tag.div class: "review_item" do
         if gift
           h.concat h.content_tag :strong, "#{gift.name} doesn't have any reviews. Be the first to write one!"
           h.concat h.button_to "Write a review", h.new_gift_review_path(gift), method:'get', class:'big_btn'
         else
           "<strong> #{user.username.humanize} hasn't written any reviews.</strong>"
         end.html_safe
+      end
+    else
+      if gift && h.current_user && !h.current_user.wrote_a_review?(gift)
+        h.button_to "Write a review", h.new_gift_review_path(gift), method:'get', class:'big_btn'
       end
     end
   end
