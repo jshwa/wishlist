c715a415a28013ad152b60a003400a36a0bf93ec
jshwa <32717724+jshwa@users.noreply.github.com>
2018-01-26 22:13:41 -0600

    take out unneeded attributes from serializer, change ams to attributes

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,4 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :stars, :comment, :user_id, :gift_id
+  attributes :id, :stars, :comment, :gift_id, :updated_at
+  belongs_to :user
 end
