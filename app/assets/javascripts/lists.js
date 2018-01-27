c7e00f2befa04a0e0e9cbb5cb57cf8bc558a0612
jshwa <32717724+jshwa@users.noreply.github.com>
2018-01-26 22:10:58 -0600

    get json from controller and display it in handlebars template

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -1,10 +1,13 @@
 function attachReviewListeners() {
-  $('.js-list-review-btn').on('click', function(e){
+  $('.js-wishlist-review-btn').on('click', function(e){
     e.preventDefault();
-    let id = $(this).attr('data-id');
-    $.getJSON(this.action).done(function(reviews){
-      $.each(reviews.data, function(key, review){
-        $(`#gift-${id}-reviews`).append(review.attributes.comment);
+    var id = $(this).attr('data-id');
+    var source = $('#wishlist-reviews-template').html();
+    var template = Handlebars.compile(source);
+    $(`#gift-${id}-reviews`).append('<h2 class="reviews_title"> Reviews: </h2>');
+    $.getJSON(this.action).done(function(response){
+      $.each(response, function(index, review){
+        $(`#gift-${id}-reviews`).append(template(review))
       });
     });
   });
