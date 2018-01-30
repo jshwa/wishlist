9fdd11b6b181c986d4b4c831e3dee3192e615c18
jshwa <32717724+jshwa@users.noreply.github.com>
2018-01-29 20:43:35 -0600

    add on_list attribute to indicate if a gift is on the current user's wishlist

--- app/serializers/gift_serializer.rb
+++ app/serializers/gift_serializer.rb
@@ -1,3 +1,8 @@
 class GiftSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :image, :price, :url
+  attributes :id, :name, :description, :image, :price, :url, :on_list
+
+  def on_list
+    current_user.list.gifts.include?(object)
+  end
+
 end
