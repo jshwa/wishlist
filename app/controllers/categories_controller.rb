0a2f5247a4ff535a209dc5a06406aa4f20934247
jshwa <32717724+jshwa@users.noreply.github.com>
2018-01-29 20:42:29 -0600

    categories controller responds to requests for html and json

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,9 +1,20 @@
 class CategoriesController < ApplicationController
+  before_action :set_categories
+
   def index
-    @categories = Category.all
   end
 
   def show
     @category = Category.find_by(id: params[:id])
+    respond_to do |format|
+      format.html
+      format.json {render json: @category, include: ['gifts','gifts.**', 'users']}
+    end
+  end
+
+  private
+
+  def set_categories
+    @categories = Category.all.order(:name)
   end
 end
