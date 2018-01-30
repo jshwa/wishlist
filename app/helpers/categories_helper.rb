03326cf07c5cdf7579655fe9d56fb5f76513013e
jshwa <32717724+jshwa@users.noreply.github.com>
2018-01-30 10:49:25 -0600

    use helper functions to display sidebar nav by names - not letters

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -4,4 +4,36 @@ module CategoriesHelper
       user if user.most_popular_categories.include?(category)
     end.uniq
   end
+
+  def display_side_az_list
+    al = ('A'..'Z').collect do |letter|
+      tag.nav class:'side_list', id:letter do
+        if list_links[letter]
+          l = list_links[letter].collect do |link|
+            link[1]
+          end
+          l[0]
+        end
+      end
+    end
+    raw al.join
+  end
+
+  def list_links
+    links = Hash.new
+    @categories.each do |category|
+      if links[category.name.slice(0)].blank?
+        links[category.name.slice(0)] = Hash.new
+        links[category.name.slice(0)][0] = link_to category.name, category_path(category), class:"category-link"
+      else
+        num = 0
+        while links[category.name.slice(0)][num] != nil do
+          num += 1
+        end
+        links[category.name.slice(0)][num] = Hash.new
+        links[category.name.slice(0)][num] = link_to category.name, category_path(category), class:"category-link"
+      end
+    end
+    links
+  end
 end
