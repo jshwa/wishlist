6b5cde7b10dc4980a26e3aa6035299140e855ec4
jshwa <32717724+jshwa@users.noreply.github.com>
2018-04-28 15:52:51 -0500

    fixed bug where category wasn't showing for not logged in user.

--- app/serializers/gift_serializer.rb
+++ app/serializers/gift_serializer.rb
@@ -2,7 +2,7 @@ class GiftSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :image, :price, :url, :on_list
 
   def on_list
-    current_user.list.gifts.include?(object)
+    current_user && current_user.list.gifts.include?(object)
   end
 
 end
